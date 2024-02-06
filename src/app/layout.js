a1b0d829dc269fce7037216bc7ace6e0eafba196
uplostudio <uplostudio@gmail.com>
2024-02-06 14:07:28 +0000

    commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 
 
 import "../../devlink/global.css";
-import { DevLinkProvider } from "../../devlink";
+import { DevLinkProvider, Navbar } from "../../devlink";
 
 import { Inter } from "next/font/google";
 import { PrismicText } from "@prismicio/react";
@@ -42,29 +42,6 @@ async function Header() {
   const navigation = await client.getSingle("navigation");
 
   return (
-    <Bounded as="header" yPadding="sm">
-      <div className="flex flex-wrap items-baseline justify-between gap-x-6 gap-y-3 leading-none">
-        <PrismicNextLink
-          href="/"
-          className="text-xl font-semibold tracking-tight"
-        >
-          <PrismicText field={settings.data.siteTitle} />
-        </PrismicNextLink>
-        <nav>
-          <ul className="flex flex-wrap gap-6 md:gap-10">
-            {navigation.data?.links.map((item) => (
-              <li
-                key={prismic.asText(item.label)}
-                className="font-semibold tracking-tight text-slate-800"
-              >
-                <PrismicNextLink field={item.link}>
-                  <PrismicText field={item.label} />
-                </PrismicNextLink>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </div>
-    </Bounded>
+    <Navbar />
   );
 }
